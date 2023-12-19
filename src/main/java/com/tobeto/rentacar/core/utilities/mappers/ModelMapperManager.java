04a6ad737d981921b9f58136fbbf58e5304b2a3b
Mert <nagodretrem@gmail.com>
2023-12-19 17:07:31 +0300

    Changed matching strategy

--- src/main/java/com/tobeto/rentacar/core/utilities/mappers/ModelMapperManager.java
+++ src/main/java/com/tobeto/rentacar/core/utilities/mappers/ModelMapperManager.java
@@ -22,7 +22,7 @@ public class ModelMapperManager implements ModelMapperService {
     @Override
     public ModelMapper forRequest() {
         this.modelMapper.getConfiguration().setAmbiguityIgnored(true)
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
         return this.modelMapper;
     }
 }
